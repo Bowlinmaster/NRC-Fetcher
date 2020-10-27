e19f087017d4c6039ebc5a6c96f9a27d8c803ed8
Drew Boatwright <MdDgDrew@aol.com>
2020-10-27 16:08:04 -0400

    Initial project and solution creation.

---
+++ NRC-Fetcher/NRC-Fetcher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRC_Fetcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
